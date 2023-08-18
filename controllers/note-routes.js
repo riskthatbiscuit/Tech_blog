ecd7e3e95815466ca6549f5c9bf0cb87350e2f9b
Casey Mcgrath <caseymcgrath1000@gmail.com>
2023-08-17 21:22:23 -0400

    Formatted comments on notes

--- controllers/note-routes.js
+++ controllers/note-routes.js
@@ -1,7 +1,5 @@
 const router = require("express").Router();
-const sequelize = require("sequelize");
 const {Notes, Comments} = require("../models");
-require("dotenv").config();
 const withAuth = require("../utils/auth");
 
 
@@ -9,29 +7,23 @@ const withAuth = require("../utils/auth");
 router.get("/:id", withAuth, async (req, res) => {
   try {
     const noteID = req.params.id;
+    
     const notesData = await Notes.findByPk(noteID);
     const note = notesData.get({ plain: true });
-    console.log("here it is!")
-    console.log(notesData)
 
     const commentsData = await Comments.findAll({
       where: {
         noteId: noteID
       }
     })
-
-    // console.log(commentsData)
     const comments = commentsData.map((comment) => comment.get({ plain: true }));
     console.log(comments)
-
-
     res.render('note', {
       note, comments,
       loggedIn: req.session.loggedIn,
     });
     return;
   } catch (err) {
-    console.log("ERROR")
     res.status(500).json(err);
   }
 });
